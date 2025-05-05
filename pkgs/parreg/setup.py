f67492e091e56c30eb13fd4a3c0753609ed832f5
yuqiong.liu <yuqiong.liu@ertcorp.com>
2025-05-05 17:41:34 -0400

    initial commit

---
+++ pkgs/parreg/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="parreg",  
+    version="0.0.1",
+    author="Yuqiong.Liu",
+    author_email="yuqiong.liu@ertcorp.com",
+    description="NextGen parameter regionalization",
+    #long_description=open("README.md").read(),
+    long_description="NextGen parameter regionalization",
+    long_description_content_type="text/markdown",
+    #url="https://github.com/yourusername/your-repo",  # Optional
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
+        "pydantic>=2.0",
+        "pyyaml",
+        # Add other dependencies here
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+) 
+
