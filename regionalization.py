c11808986a0e56909fa6b16672d02d0f29ed2873
Matthew.Deshotel <mdeshotel@dewberry.com>
2025-06-16 09:58:30 -0500

    remove duplicate lines

--- regionalization.py
+++ regionalization.py
@@ -9,14 +9,13 @@ import logging
 from contextlib import contextmanager
 from pathlib import Path
 from time import time
-from time import time
 
 import parreg.process_config as pc
 from parreg.logging_config import setup_logging
 
-
 setup_logging()
 logger = logging.getLogger(__name__)
+
 
 # function to timing the execution of various steps
 @contextmanager
@@ -28,13 +27,16 @@ def timing_block(step_str: str):
     logger.info(f"  Execution time for {step_str}: {end - start} seconds")
 
 
-
 if __name__ == "__main__":
     # Create the parser
     parser = argparse.ArgumentParser()
 
     # Add arguments
-    parser.add_argument("config_file", type=str, help="Path to the config yaml file for parameter regionalization")
+    parser.add_argument(
+        "config_file",
+        type=str,
+        help="Path to the config yaml file for parameter regionalization",
+    )
 
     # Parse the arguments
     args = parser.parse_args()
@@ -50,9 +52,7 @@ if __name__ == "__main__":
 
     # process by VPU
     for vpu in config.general.vpu_list:
-    for vpu in config.general.vpu_list:
         # get receivers and qualified donors in the VPU, and compute pairwise spatial distances between them
-        with timing_block("get_donors_receivers"):
         with timing_block("get_donors_receivers"):
             donors, receivers, df_dist_spatial = pc.get_donors_receivers(config, vpu)
 
@@ -68,6 +68,5 @@ if __name__ == "__main__":
         )
 
         # loop through regionalization algorithms to generate donor-receiver pairings
-        with timing_block("generate_pairing"):
         with timing_block("generate_pairing"):
             pc.generate_pairing(config, vpu, df_attrs_all, df_dist_spatial)
