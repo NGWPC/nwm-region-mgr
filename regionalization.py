2eba1d4f59a61b77fe61e05a7f6d758a0876cef3
yuqiong.liu <yuqiong.liu@ertcorp.com>
2025-06-27 17:54:12 -0400

    add formulation regionalization package formreg

--- regionalization.py
+++ regionalization.py
@@ -44,7 +44,6 @@ if __name__ == "__main__":
 
     # read and validate config
     config_file = Path(args.config_file)
-    config_file = Path("configs/config.yaml")
     if not config_file.exists():
         raise FileNotFoundError(config_file)
 
@@ -58,14 +57,10 @@ if __name__ == "__main__":
 
         # assemble the attribute data for donors and receivers
         with timing_block("process_attr_data"):
-            donors, receivers, df_attrs_all = pc.process_attr_data(
-                config, vpu, donors, receivers, df_dist_spatial
-            )
+            donors, receivers, df_attrs_all = pc.process_attr_data(config, vpu, donors, receivers, df_dist_spatial)
 
         # detemine whether the catchments are snowy (as snowy and non-snowy catchments are processed separately)
-        df_attrs_all = pc.set_snow_flag(
-            df_attrs_all, config.algorithms.general.min_snow_frac
-        )
+        df_attrs_all = pc.set_snow_flag(df_attrs_all, config.algorithms.general.min_snow_frac)
 
         # loop through regionalization algorithms to generate donor-receiver pairings
         with timing_block("generate_pairing"):
