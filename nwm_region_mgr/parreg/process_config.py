8e075ce5cd3743fc147afac1e97395ce70bb1cfa
Yuqiong Liu <yuqiong.liu@U-28HBMJQ9PK1NS.nextgenwaterprediction.com>
2025-12-12 09:55:59 -0500

    remove get_formulation_file_name

--- nwm_region_mgr/parreg/process_config.py
+++ nwm_region_mgr/parreg/process_config.py
@@ -75,17 +75,6 @@ class ParameterRegionalizationProcessor(BaseConfigProcessor):
         if "formulation_dict" in self.__dict__:
             del self.__dict__["formulation_dict"]
 
-    def get_formulation_file_name(self, vpu: str, config: Any) -> str:
-        """Get the formulation file name for a given VPU."""
-        co = getattr(config.output, "formulation", None)
-        if co is None:
-            logger.warning(
-                "No 'formulation' section found in formulation output config."
-            )
-            return ""
-
-        return co.get_file_path(vpu=vpu)
-
     def run_parreg_for_vpu(self, vpu: str, frp: FRP) -> None:
         """Run the parameter regionalization process for a given VPU."""
         # set the VPU for processing
@@ -141,7 +130,7 @@ class ParameterRegionalizationProcessor(BaseConfigProcessor):
 
         # generate pairings
         with self.timing_block("generate_pairing"):
-            self.generate_pairing(df_attr_all, self.dist_spatial, frp.config)
+            self.generate_pairing(df_attr_all, self.dist_spatial, frp)
 
         logger.info(f"Parameter regionalization for VPU {vpu} completed.")
 
@@ -556,11 +545,15 @@ class ParameterRegionalizationProcessor(BaseConfigProcessor):
             f"donor_receiver_dist_{self.config.general.domain}_vpu{self.vpu}.{out.format}",
         )
 
-    def set_formulation_dict(self, form_config: Any):
+    def set_formulation_dict(self, frp: FRP):
         """Get the formulation lookup table."""
         df_form_all = pd.DataFrame()
         for vpu in self.donor_vpus:
-            formulation_file = self.get_formulation_file_name(vpu, form_config)
+            formulation_file = frp.get_output_file_path(
+                "formulation",
+                vpu,
+                use_stem_suffix=False,
+            )
 
             if not formulation_file:
                 msg = f"No formulation file found for VPU {vpu}. Exiting ..."
@@ -1358,7 +1351,7 @@ class ParameterRegionalizationProcessor(BaseConfigProcessor):
             )
 
     def generate_pairing(
-        self, df_attr_all: pd.DataFrame, dist_spatial: pd.DataFrame, form_config: Any
+        self, df_attr_all: pd.DataFrame, dist_spatial: pd.DataFrame, frp: FRP
     ):
         """Conduct donor-receiver pairing for a given VPU based on the configuration.
 
@@ -1368,7 +1361,7 @@ class ParameterRegionalizationProcessor(BaseConfigProcessor):
         Args:
             df_attr_all: dataframe containing the full attribute data for all receivers and donors
             dist_spatial: dataframe containing the pair-wise spatial distance between donors and receivers
-            form_config: configuration object containing the settings for formulation regionalization
+            frp: FRP object containing the settings for formulation regionalization
 
         Returns:
             None, but saves the pairing results to a file.
@@ -1404,7 +1397,7 @@ class ParameterRegionalizationProcessor(BaseConfigProcessor):
 
                 # loop through formulations to conduct pairing separately for each formulation
                 df_pairs_all = pd.DataFrame()
-                self.set_formulation_dict(form_config)
+                self.set_formulation_dict(frp)
                 for form_key, form_values in self.formulation_dict.items():
                     # identify donors and receivers for the current formulation
                     donors_in_form, receivers_in_form = (
@@ -1470,7 +1463,7 @@ class ParameterRegionalizationProcessor(BaseConfigProcessor):
 
                 # create formulation parameter file
                 self.create_formulation_parameter_file(
-                    getattr(form_config.output, "formulation", None), pairer_name
+                    getattr(frp.config.output, "formulation", None), pairer_name
                 )
 
                 end_time = time.time()
