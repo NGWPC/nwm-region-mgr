f5171c89e52f4c168e709554e5d7be1a1868012b
Yuqiong Liu <yuqiong.liu@U-28HBMJQ9PK1NS.nextgenwaterprediction.com>
2026-05-11 12:51:42 -0400

    update util script add_sft_to_stats_params_files.py for more processing

--- util_scripts/add_sft_to_stats_params_files.py
+++ util_scripts/add_sft_to_stats_params_files.py
@@ -1,5 +1,11 @@
+"""Script to add modules to formulation column in stats and params files.
+
+Also renames columns in stats files to match ngenCERF format and adds random calibration_run_id and validation_run_id columns.
+"""
+
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 stat_dir = Path("../data/inputs/region/calval_stats")
@@ -55,7 +61,57 @@ def update_formulation(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def process_files(directory: Path, file_pattern: str, extension: str):
+def rename_columns(
+    df: pd.DataFrame,
+) -> pd.DataFrame:  # Mapping of old column names to new column names
+    column_name_map = {
+        "csi": "CSI",
+        "cor": "Corr",
+        "event_volume_bias": "EVBIAS",
+        "far": "FAR",
+        "kge": "KGE",
+        "nse": "NSE",
+        "nselog": "NSELog",
+        "nseWt": "NSEWt",
+        "bias": "PBIAS",
+        "peak_bias": "PKBIAS",
+        "peak_tm_err_hr": "PKTE",
+        "pod": "POD",
+        "rmse": "RMSE",
+    }
+
+    # Rename columns
+    df = df.rename(columns=column_name_map)
+
+    # columns to keep
+    cols_to_keep = ["formulation", "gage_id", "evalPeriod"] + list(
+        column_name_map.values()
+    )
+    df = df[cols_to_keep]
+
+    return df
+
+
+def add_job_ids(df: pd.DataFrame) -> pd.DataFrame:
+    """Add random calibration_run_id and validation_run_id columns to the dataframe."""
+    n_rows = len(df)
+    df["calibration_run_id"] = np.random.randint(10000, 100000, size=n_rows)
+    df["validation_run_id"] = np.random.randint(10000, 100000, size=n_rows)
+
+    # Move the two columns to the front
+    front_cols = ["calibration_run_id", "validation_run_id"]
+    df = df[front_cols + [col for col in df.columns if col not in front_cols]]
+
+    return df
+
+
+def process_files(
+    directory: Path,
+    file_pattern: str,
+    extension: str,
+    extension_new: str,
+    type_str: str,
+):
     """Process files in the specified directory matching the pattern and extension."""
     for domain in domains:
         file = directory / f"{file_pattern}_{domain}.{extension}"
@@ -77,19 +133,29 @@ def process_files(directory: Path, file_pattern: str, extension: str):
         # replace formulation
         df = update_formulation(df)
 
-        # write
-        out_file = directory / f"{file_pattern}_{domain}_{append_str}.{extension}"
+        # rename columns
+        if type_str == "stats":
+            df = rename_columns(df)
 
-        if extension == "parquet":
+        # add job ids
+        df = add_job_ids(df)
+
+        # loop through columns and convert to -9999 to NaN
+        df = df.replace(-9999, "nan")
+
+        # write
+        out_file = directory / f"{file_pattern}_{domain}_{append_str}.{extension_new}"
+
+        if extension_new == "parquet":
             df.to_parquet(out_file, index=False)
-        elif extension == "csv":
+        elif extension_new == "csv":
             df.to_csv(out_file, index=False)
 
 
 if __name__ == "__main__":
     """Add modules to formulation column in stats and params files."""
     # stats
-    process_files(stat_dir, stat_file_pattern, "parquet")
+    process_files(stat_dir, stat_file_pattern, "parquet", "parquet", "stats")
 
     # params
-    process_files(param_dir, param_file_pattern, "csv")
+    process_files(param_dir, param_file_pattern, "csv", "csv", "params")
